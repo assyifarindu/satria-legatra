f1f564dc03b10815ad6acecc2d004a32398b0dd6
mstkm <mustakimmugi@gmail.com>
2026-08-31 13:23:05 +0700

    feat: add condition for show button Continue to Drafting

--- resources/views/tsp/request-document/tracking.blade.php
+++ resources/views/tsp/request-document/tracking.blade.php
@@ -64,7 +64,9 @@
                                             <div class="row mt-4">
                                                 <div class="col-sm-6">
                                                 </div> <!-- end col -->
-                                                @if ($requestDocument->stage_id == 1 && getRoles(Auth::user()->id) === 'Admin Legal TSP')
+                                                @if (
+                                                    ($requestDocument->stage_id == 1 || $requestDocument->stage_id == 2) && getRoles(Auth::user()->id) === 'Admin Legal TSP'
+                                                )
                                                     <div class="col-sm-6">
                                                         <div class="text-sm-end mt-2 mt-sm-0">
                                                             <a href="{{ route('tsp.request-document.legal-drafting', $requestDocument->id) }}"
